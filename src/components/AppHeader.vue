428d6d162369d725e39a705b470e29733eccd941
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-19 15:19:53 +0200

    removed default componenets, added main and header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+        
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
