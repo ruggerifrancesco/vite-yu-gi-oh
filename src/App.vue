78ea01db0277b6dd6d7624dbdd4a60daa881f7b2
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-19 15:26:41 +0200

    defined all strcurtres for js componenets, added style files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
+  <AppHeader />
+  <AppMain />
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMainVue from './components/AppMain.vue';
+
 export default {
     name: 'App',
+    data() {
+        return {
+
+        }
+    },
+    components: {
+      AppHeader,
+      AppMainVue
+    }
 }
 </script>
 
 <style lang="scss">
-
+@import "bootstrap/scss/bootstrap";
+@import './styles/general.scss';
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,11 @@
 <script>
 export default {
     name: 'AppHeader',
+    data() {
+        return {
+            
+        }
+    },
 }
 </script>
 
