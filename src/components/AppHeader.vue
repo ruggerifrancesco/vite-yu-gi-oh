368f9beb502b14a193395f13b8c3be2498bcda53
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-19 15:47:15 +0200

    Defined simple nav bar, added logo and title

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,18 @@
 <template>
     <header>
-        
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+
+                    <div class="logo-wrapper">
+                        <img src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es"
+                           alt="Yu Gi Oh Logo">
+                    </div>
+
+                    <h1>Yu Gi Oh Api</h1>
+                </a>
+            </div>
+        </nav>
     </header>
 </template>
 
@@ -16,5 +28,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    
+@use '../styles/partials/_mixins.scss' as *;
+@use '../styles/partials/variables.scss' as *;
+    .logo-wrapper {
+        width: 6rem;
+    }
+
+    .navbar-brand {
+        @include flex (row, flex-start, center)
+    }
 </style>
