a8addc0a3c55e8c4e3e8985d237154fcb6b96829
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-19 16:14:46 +0200

    defined axios get method from api call, now append all cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,35 @@
 <template>
     <main>
-        <SingleCard />
+        <SingleCard v-for="card in cardsList"/>
     </main>
 </template>
 
 <script>
+import axios from 'axios';
 import { store } from '../store.js';
 import SingleCard from './SingleCard.vue';
 
 export default {
     name: 'AppMain',
+    components: {
+        SingleCard
+    },
     data() {
         return {
             store,
+            cardsList: [],
         }
     },
-    components: {
-        SingleCard
-    }
+    created() {
+        axios.get(store.yuGiOhApi)
+        .then( (response) => {
+            console.log(response.data);
+            this.cardsList = response.data
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    },
 }
 </script>
 
