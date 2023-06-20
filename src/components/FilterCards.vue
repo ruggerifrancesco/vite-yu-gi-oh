bfba32254bcdc39bbc0699fc6f7146e2d9862bfe
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-20 13:07:35 +0200

    added a default value

--- src/components/FilterCards.vue
+++ src/components/FilterCards.vue
@@ -1,6 +1,7 @@
 <template>
     <section>
         <select>
+            <option value="default-value">Scegli un filtro!</option>
             <option value="archetype-value" v-for="(archetype, index) in archetypesArray.slice(0, 15)" :key="index">
                 {{ archetype.archetype_name }}
             </option>
