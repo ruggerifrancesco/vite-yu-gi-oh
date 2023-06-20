6ddf2aaa1cdceeb95f509b4c2decb48f00be56f5
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-20 12:41:29 +0200

    chnaged api result in 50 cards

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive ({
-    searchText: 'ciao',
     loading: true,
-    yuGiOhApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    yuGiOhApi: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0',
+    archetypesApi: 'https://db.ygoprodeck.com/api/v7/archetypes.php'
 })
